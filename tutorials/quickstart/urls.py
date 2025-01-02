37fb34991eb8e010b7454fcbe4517a7b13a1fe1b
Saad Ahmed <saadmakhdoom12@gmail.com>
2025-01-02 23:20:56 +0500

    second commit

--- tutorials/quickstart/urls.py
+++ tutorials/quickstart/urls.py
@@ -2,11 +2,26 @@ from django.urls import path
 from quickstart import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
-    path("quickstart/", views.SnippetList.as_view()),
-    path("quickstart/<int:pk>/", views.SnippetDetail.as_view()),
-    path("users/", views.UserList.as_view()),
-    path("users/<int:pk>/", views.UserDetail.as_view()),
-]
+# API endpoints
+urlpatterns = format_suffix_patterns(
+    [
+        path("", views.api_root),
+        path("quickstart/", views.SnippetList.as_view(), name="snippet-list"),
+        path(
+            "quickstart/<int:pk>/",
+            views.SnippetDetail.as_view(),
+            name="snippet-detail",
+        ),
+        path(
+            "quickstart/<int:pk>/highlight/",
+            views.SnippetHighlight.as_view(),
+            name="snippet-highlight",
+        ),
+        path("users/", views.UserList.as_view(), name="user-list"),
+        path(
+            "users/<int:pk>/", views.UserDetail.as_view(), name="user-detail"
+        ),
+    ]
+)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- tutorials/quickstart/views.py
+++ tutorials/quickstart/views.py
@@ -5,6 +5,31 @@ from quickstart.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics, permissions, viewsets
 from tutorial.quickstart.serializers import GroupSerializer
 from quickstart.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "users": reverse("user-list", request=request, format=format),
+            "snippets": reverse(
+                "snippet-list", request=request, format=format
+            ),
+        }
+    )
 
 
 class SnippetList(generics.ListCreateAPIView):
